e297dec0f1999ae6be0e8c8b4c0a3ded61655513
Alice R <alicepr@yandex.ru>
2023-12-01 22:01:50 +0300

    hw5: create and import global styles, add fonts and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RestaurantsPage } from './pages/restaurants-page/component';
 import { Restaurant } from './types/types';
+import './styles/index.css';
 
 interface AppProps {
   restaurants: Restaurant[];
