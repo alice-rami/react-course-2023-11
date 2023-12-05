e53b1141914a0ac0e8269d86f542a2709d34bb36
Alice R <alicepr@yandex.ru>
2023-12-05 19:24:37 +0300

    hw7: remove unused types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,29 +1,8 @@
-export type Dish = {
-  id: string;
-  name: string;
-  price: number;
-  ingredients: string[];
-};
-
-export type Review = {
-  id: string;
-  user: string;
-  text: string;
-  rating: number;
-};
-
 export type NormalizedReview = {
   id: string;
   userId: string;
   text: string;
   rating: number;
-};
-
-export type Restaurant = {
-  id: string;
-  name: string;
-  menu: Dish[];
-  reviews: Review[];
 };
 
 export type NormalizedRestaurant = {
